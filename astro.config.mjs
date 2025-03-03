43f9034bbb01d06c26daf51a7646057dac7a3e9d
Rahul Rahate <rrahate@godaddy.com>
2025-03-03 12:06:35 -0600

    Fix build and deploy configuration 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-    site: 'rrahate-godaddy.github.io',
+    site: 'https://rrahate-godaddy.github.io',
     base: '/test-gh',
     outDir: 'dist', // Ensure this is set to 'dist'
 });
